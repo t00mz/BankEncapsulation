a0c5438b1fd1bc7c42f187c1c0fab55c1bdeb09a
Michael Hatoum <hatoumm89@gmail.com>
2022-10-28 14:17:53 -0400

    done

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myBankAcc = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+            
+            var depAmount = double.Parse(Console.ReadLine());
+
+            myBankAcc.Deposit(depAmount);
+
+            Console.WriteLine($"Your balance is now ${myBankAcc.GetBalance()}");
         }
     }
 }
